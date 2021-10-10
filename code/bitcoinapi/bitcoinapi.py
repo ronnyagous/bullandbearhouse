04b7ea05031230bf648e2d688c099339e77bc4e7
ronnyagous <github.com@vanderham.org>
2021-10-10 18:46:27 +0200

    Added response to 429  error (Too Many Requests)
    With a 429 error, there are 10 subsequent retries. First one after 125ms, the second after 250ms, the third after 500ms etc.

--- code/bitcoinapi/bitcoinapi.py
+++ code/bitcoinapi/bitcoinapi.py
@@ -1,5 +1,6 @@
 import urllib.request, json 
 from datetime import datetime, timedelta
+import time
 
 import logging
 
@@ -11,47 +12,50 @@ class bbh_bitcoinapi:
         logging.debug(f'{self.apiBaseUrl}')
 
     def currentPrice(self, coin):
-        result = None
         reqUrl = f'{self.apiBaseUrl}/assets/{coin}'
+        return self.loadData(reqUrl)
 
-        logging.debug(reqUrl)
-
-        request = urllib.request.Request(reqUrl, headers={'User-Agent': 'Mozilla/5.0'})
-        with urllib.request.urlopen(request) as response:
-            codedData = json.loads(response.read())
-            result = codedData['data']
-
-        return result
 
     def rate(self, currency):
-        result = None
         reqUrl = f'{self.apiBaseUrl}/rates/{currency}'
-
-        logging.debug(reqUrl)
-
-        request = urllib.request.Request(reqUrl, headers={'User-Agent': 'Mozilla/5.0'})
-        with urllib.request.urlopen(request) as response:
-            codedData = json.loads(response.read())
-            result = codedData['data']
-
-        return result
+        return self.loadData(reqUrl)
 
 
     def history(self, coin):
-        result = None
 
         interval = 'm5'
         starttime = int(datetime.timestamp(datetime.now() - timedelta(hours=24))) * 1000
         endtime = int(datetime.timestamp(datetime.now())) * 1000
 
         reqUrl = f'{self.apiBaseUrl}/assets/{coin}/history?interval={interval}&start={starttime}&end={endtime}'
+        return self.loadData(reqUrl)
 
+
+    def loadData(self, reqUrl):
+        result = None
         logging.debug(reqUrl)
 
-        req = urllib.request.Request(reqUrl, headers={'User-Agent': 'Mozilla/5.0'})
-        with urllib.request.urlopen(req) as url:
-            codedData = json.loads(url.read())
-            result = codedData['data']
+        maxRetry = 10
+        retryInterval = 125
+
+        
+        for retry in range(1, maxRetry):
+
+            try:
+                req = urllib.request.Request(reqUrl, headers={'User-Agent': 'Mozilla/5.0'})
+                with urllib.request.urlopen(req) as url:
+                    codedData = json.loads(url.read())
+                    result = codedData['data']
+            except urllib.error.HTTPError as e:
+                if (e.getcode() == 429):
+                    logging.debug(f'Too many request. Pausing {retryInterval} ms.')
+                    time.sleep(retryInterval / 1000)
+                    retryInterval = retryInterval * 2
+                else:
+                    logging.error(e)
+
+            if (result != None): break
+
+
 
         return result
-
